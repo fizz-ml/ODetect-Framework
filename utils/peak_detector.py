acf8506125c8d5c00df111743f456b874138ea1d
Curtis <mathcurt@gmail.com>
2018-03-16 18:27:29 -0700

    breath_ccn feature extractor

--- utils/peak_detector.py
+++ utils/peak_detector.py
@@ -5,17 +5,24 @@ from scipy.fftpack import fft,ifft
 
 def filter_signal(signal,sampling_rate,high_freq,low_freq):
     n  = len(signal)
-    hf_index = int(np.ceil(n*high_freq/sampling_rate))
-    lf_index = int(np.floor(n*low_freq/sampling_rate))
+    hf_index = int(np.ceil(n*high_freq/sampling_rate/2))
+    lf_index = int(np.floor(n*low_freq/sampling_rate/2))
     signal_fft = fft(signal)
     filter_fft = signal_fft
     filter_fft[hf_index:] = 0
     filter_fft[:lf_index] = 0
     filtered_signal = ifft(filter_fft)
     return filtered_signal 
+
 def main():
+    data = np.loadtxt("../data/exp_009.csv", delimiter =",")[:,1]
+    data_n = (data-np.mean(data))/np.std(data)*3
+    data_n = -1*filter_signal(data_n,200,3,0.05)
+    plt.plot(data_n)
     data = np.loadtxt("../data/exp_009.csv", delimiter =",")[:,3]
-    fs = filter_signal(data,200,0.5,0.05)
+    data_n = (data-np.mean(data))/np.std(data)
+
+    fs = filter_signal(data_n,200,1,0.1)
     a = fs[:-2]
     b = fs[1:-1]
     c = fs[2:]
@@ -24,7 +31,6 @@ def main():
     peak_amplitudes = fs[peaks]
     troughs_amplitudes = fs[troughs]
     plt.plot(fs)
-    plt.plot(data-np.mean(data))
     plt.scatter(peaks,peak_amplitudes)
     plt.scatter(troughs,troughs_amplitudes)
     plt.show()
