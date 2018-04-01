6c133606f5995cf73c20aa24eede483b45ef6385
Curtis <mathcurt@gmail.com>
2018-04-01 16:16:54 -0700

    added an identity feature

--- features/breath_cnn_feature.py
+++ features/breath_cnn_feature.py
@@ -6,18 +6,20 @@ import torch.nn.functional as f
 import torch as t
 from torch import FloatTensor as FT
 from torch.autograd import Variable as V
+import os
 
 class BreathCNNFeature(WindowFeature,TrainableFeature):
     def __init__(self,sampling_rate,parameter_dict):
-        super(BreathCNNFeature).__init__(sampling_rate,parameter_dict)
-        self._cuda = torch.cuda.is_available()
+        super(BreathCNNFeature,self).__init__(sampling_rate,parameter_dict)
+        self._cuda = t.cuda.is_available()
         self._cnn = BreathCNN(
                 len(self._in_features),
                 self._channel_count,
+                self._feild,
                 self._dilation,
-                self._channel,
+                self._layers,
                 )
-        if self._cuda():
+        if self._cuda:
             self._cnn.cuda()
 
     def calc_feature(self, window):
@@ -38,9 +40,9 @@ class BreathCNNFeature(WindowFeature,TrainableFeature):
 
     def _process_label(self, y_window):
         ds_window = y_window[::self._down_sample]
-        data_n = ((data-np.mean(data))/np.std(data))[window]
-        spline_filtered_data = SimpleSplineFilter(avg_win=int(20/200*self._samping_rate),ds=int(20/200*self._samping_rate),s=int(45/200*self._samping_rate)).calc_feature(data_n)
-        thermistor = SimpleLocalNorm(self._samping_rate,{"local_window_lenght",40}).calc_feature(spline_filtered_data)
+        data_n = ((ds_window-np.mean(ds_window))/np.std(ds_window))
+        spline_filtered_data = SimpleSplineFilter(avg_win=int(20/200*self._sampling_rate),ds=int(20/200*self._sampling_rate),s=int(45/200*self._sampling_rate)).calc_feature(data_n)
+        thermistor = SimpleLocalNorm(self._sampling_rate,{"local_window_lenght":40}).calc_feature(spline_filtered_data)
         label = V(FT(np.expand_dims(np.expand_dims((thermistor,0),0))))
         if self._cuda:
             label = label.cuda()
@@ -51,8 +53,8 @@ class BreathCNNFeature(WindowFeature,TrainableFeature):
         t.manual_seed(7)
         np.random.seed(7)
 
-        data_x_train_list = [self._process_inputs(x) for x in train_x]
-        data_y_train_list = [self._process_labels(x) for y in train_y]
+        data_x_train_list = [self._process_input(x) for x in train_x]
+        data_y_train_list = [self._process_label(y) for y in train_y]
 
         def closure():
             opt.zero_grad()
@@ -78,8 +80,8 @@ class BreathCNNFeature(WindowFeature,TrainableFeature):
 
         torch.save(self._cnn.state_dict,self._param_path)
 
-    def set_params(self):
-        super(BreathCNNFeature,self).set_params()
+    def set_params(self, params):
+        super(BreathCNNFeature,self).set_params(params)
         if os.path.exists(self._param_path):
             self._cnn.load_state_dict(torch.load(self._param_path))
 
@@ -97,8 +99,10 @@ class BreathCNNFeature(WindowFeature,TrainableFeature):
             "in_features" : (list,"list of feature objects that will go into the cnn"),
             "down_sample" : (int,"factor by which to down_sample the input signal"),
             "train_time" : (int,"how many iterations to train the cnn"),
-            "learning_rate" : (int,"learning rate for the cnn")
+            "learning_rate" : (int,"learning rate for the cnn"),
+            "param_path" : (str, "path in which to save parameters")
         }
+        return param_template
 
 
 class BreathCNN(nn.Module):
@@ -120,7 +124,7 @@ class BreathCNN(nn.Module):
         self._layers = nn.ModuleList()
         self._layers.append(nn.Conv1d(6,n*2,dil-1,padding=dil//2-1))
         self._layers.append(nn.Conv1d(n*2,n,dil-1,padding=dil//2-1))
-        for i in range(self._layers):
+        for i in range(layers):
             self._layers.append(nn.Conv1d(n,n,k,dilation=dil,padding=pad))
             self._layers.append(nn.Conv1d(n,n,1))
         self.out = nn.Conv1d(n,2,1)
@@ -133,7 +137,7 @@ class BreathCNN(nn.Module):
         return out
 
 def main_test():
-    features = SimpleLocalNorm(200,40)
+    features = [IdentityWindowFeature(200,{})]
     param_template = {
         "channel_count" : 10,
         "dilation" : 10,
@@ -142,9 +146,14 @@ def main_test():
         "in_features" : features,
         "down_sample" : 12,
         "train_time" : 500,
-        "learning_rate" : 0.002
+        "learning_rate" : 0.002,
+        "param_path" : "bla.t7"
     }
-    BreathCNNFeature(200,)
+    BreathCNNFeature(200,param_template)
+    train_data = [np.random.randn(100000)]*10
+    test_data = [np.random.randn(100000)]*10
+    BreathCNNFeature(200,param_template).train(train_data,test_data,None,None)
+
 
 if __name__ == "__main__":
     main_test()
